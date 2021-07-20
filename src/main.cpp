5f7f928005988c4ae8e67dd6fc0510986f7b7fd1
Jiaxin Shou <shoujiaxin@gmail.com>
2021-07-20 21:10:32 +0800

    feat: draw first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -44,12 +44,77 @@ int main() {
     return -1;
   }
 
+  // 顶点着色器
+  const char* vertex_shader_source =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main() {\n"
+      "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\n";
+  auto vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+  glCompileShader(vertex_shader);
+  int success;
+  char info_log[512];
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, nullptr, info_log);
+    std::cerr << info_log << std::endl;
+  }
+
+  // 片段着色器
+  const char* fragment_shader_source =
+      "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main() {\n"
+      "  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+      "}\n";
+  auto fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+  glCompileShader(fragment_shader);
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment_shader, 512, nullptr, info_log);
+    std::cerr << info_log << std::endl;
+  }
+
+  // 着色器程序
+  auto shader_program = glad_glCreateProgram();
+  glAttachShader(shader_program, vertex_shader);
+  glAttachShader(shader_program, fragment_shader);
+  glLinkProgram(shader_program);
+  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader_program, 512, nullptr, info_log);
+    std::cerr << info_log << std::endl;
+  }
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+
+  // 顶点数组对象
+  unsigned int vertex_array_object;
+  glGenVertexArrays(1, &vertex_array_object);
+  glBindVertexArray(vertex_array_object);
+
+  // 顶点数据
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  unsigned int vertex_buffer_object;  // 顶点缓冲对象
+  glGenBuffers(1, &vertex_buffer_object);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
   // 渲染循环 (render loop)
   while (!glfwWindowShouldClose(window)) {
     HandleInput(window);
 
     glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program);
+    glBindVertexArray(vertex_array_object);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);  // 交换颜色缓冲
     glfwPollEvents();  // 检查触发事件（键盘输入、鼠标移动等）
