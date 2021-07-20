a1f24df4d89a5faa83b532dd1664dc7a97c50f60
Jiaxin Shou <shoujiaxin@gmail.com>
2021-07-20 16:25:07 +0800

    feat: add window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
 #include <iostream>
 
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void HandleInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // 初始化 GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  // 创建窗口
+  auto window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+  // 初始化 GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // 渲染循环 (render loop)
+  while (!glfwWindowShouldClose(window)) {
+    HandleInput(window);
+
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);  // 交换颜色缓冲
+    glfwPollEvents();  // 检查触发事件（键盘输入、鼠标移动等）
+  }
+
+  glfwTerminate();
   return 0;
 }
