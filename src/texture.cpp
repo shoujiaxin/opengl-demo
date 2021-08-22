b8da8150df852b837ad575fbb8c7541c6330a1a6
Jiaxin Shou <shoujiaxin@gmail.com>
2021-08-22 21:26:24 +0800

    fix: png texture display error

--- src/texture.cpp
+++ src/texture.cpp
@@ -13,6 +13,7 @@
 
 Texture::Texture(const std::string &path) {
   int channels;
+  stbi_set_flip_vertically_on_load(true);
   const auto data = stbi_load(path.c_str(), &width_, &height_, &channels, 0);
   if (data == nullptr) {
     std::cerr << "Failed to load texture" << std::endl;
@@ -24,7 +25,8 @@ Texture::Texture(const std::string &path) {
   // 绑定纹理
   glBindTexture(GL_TEXTURE_2D, id_);
   // 生成纹理
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width_, height_, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  const auto format = (path.substr(path.size() - 4, 4) == ".png") ? GL_RGBA : GL_RGB;
+  glTexImage2D(GL_TEXTURE_2D, 0, format, width_, height_, 0, format, GL_UNSIGNED_BYTE, data);
   // 生成多级渐远纹理
   glGenerateMipmap(GL_TEXTURE_2D);
 
